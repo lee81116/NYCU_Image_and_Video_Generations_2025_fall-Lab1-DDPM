9b62fa01e47160d9fe92b5429dfe7510911cffe2
Nian-Chia <103125276+lee81116@users.noreply.github.com>
2025-10-01 00:23:55 +0800

    trivial

--- image_diffusion_todo/scheduler.py
+++ image_diffusion_todo/scheduler.py
@@ -48,7 +48,8 @@ class BaseScheduler(nn.Module):
                 t1 = i / num_train_timesteps
                 t2 = (i + 1) / num_train_timesteps
                 betas.append(1.0 - fn(t2) / fn(t1))
-            return torch.tensor(betas, dtype=torch.float32).clamp_max(beta_T)
+            beta = torch.tensor(betas, dtype=torch.float32).clamp_max(beta_T)
+            return beta
             #######################
         else:
             raise NotImplementedError(f"{mode} is not implemented.")
